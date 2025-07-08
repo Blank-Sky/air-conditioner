0c12197c2b620c6084aa6c7c219b403da16f2cb4
GuxiaoZhang <zhangguxiao@industics.com>
2025-07-08 12:55:13 +0800

    disable ad

--- packages/react/src/components/ac/AirConditioner.tsx
+++ packages/react/src/components/ac/AirConditioner.tsx
@@ -38,8 +38,8 @@ const AcLogo: React.FC = () => {
     <div className="text-center mt-28">
       <a
         className="block"
-        href={pkg.repository.url}
-        title={pkg.description}
+        // href={pkg.repository.url}
+        // title={pkg.description}
         target="_blank"
         rel="noreferrer noopener"
       >
